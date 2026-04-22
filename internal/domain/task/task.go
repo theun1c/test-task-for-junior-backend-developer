7b5714da93bc5d6b504bd4ec45b64e9baf71ac93
theun1c <belovdima1122@yandex.ru>
2026-04-23 00:30:14 +0300

    feat: upd task model - add periodicity

--- internal/domain/task/task.go
+++ internal/domain/task/task.go
@@ -11,12 +11,13 @@ const (
 )
 
 type Task struct {
-	ID          int64     `json:"id"`
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	Status      Status    `json:"status"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	ID                  int64                `json:"id"`
+	Title               string               `json:"title"`
+	Description         string               `json:"description"`
+	Status              Status               `json:"status"`
+	PeriodicitySettings *PeriodicitySettings `json:"periodicity_settings,omitempty"`
+	CreatedAt           time.Time            `json:"created_at"`
+	UpdatedAt           time.Time            `json:"updated_at"`
 }
 
 func (s Status) Valid() bool {
